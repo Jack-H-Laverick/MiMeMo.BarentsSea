0f212d987408c3cf081aff62e1477e188e1fc6f1
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-10-05 17:13:55 +0100

    Coded a conversion factor from DIN to Ammonia and Nitrate.

---
+++ R scripts/strathE2E.01 DIN FIX.R
@@ -0,0 +1,50 @@
+
+#### Setup ####
+
+rm(list=ls(all.names = TRUE))                                               # Wipe the brain
+
+packages <- c("MiMeMo.tools", "tidyverse", "sf")
+lapply(packages, library, character.only = TRUE)                            # Load packages
+
+domains <- readRDS("./Objects/Domains.rds")                                 # Import model domain
+
+headers <- read.csv("./Data/BODC/JR016-006_CTD_nutrient_data_summary_BODC_fixed.csv", nrows=2, header=FALSE)
+headers_names <- sapply(headers,paste,collapse="_")                         # Rebuild column names because they're split over 2 rows
+
+data <- read.csv(file="./Data/BODC/JR016-006_CTD_nutrient_data_summary_BODC_fixed.csv", skip = 2, 
+                 header=FALSE)
+
+names(data) <- headers_names                                                # Fix awkward header names
+
+data <- cbind(data[,1:8], select(data, starts_with(c("Ammonium", "Nitrate")))) %>% 
+  drop_na() %>%                                                             # Only use complete cases
+  st_as_sf(coords = c("Longitude (E)_","Latitude (N)_"), crs = 4326) %>%    # Convert to sf for spatial filter later
+  st_transform(crs = 3035) %>%  
+  group_by(`CTD cast_`) %>%                                                 # Per cast
+  arrange(`Depth (m)_`, .by_group = TRUE)                                   # Order depths ascending
+  
+#### Calculate ratios ####
+
+shallow_ratios <- data %>% 
+  mutate(weights = calculate_depth_share(`Depth (m)_`, min_depth = 0, max_depth = 60), # Calculate share of the depth column per sample
+         Depth = "Shallow")                                                 # Label depth layer 
+
+deep_ratios <- data %>% 
+  mutate(weights = calculate_depth_share(`Depth (m)_`, min_depth = 60, max_depth = 400), # Calculate share of the depth column per sample
+         Depth = "Deep")                                                    # Label depth layer 
+
+final <- rbind(shallow_ratios, deep_ratios) %>%                             # Combine estimates
+  filter(weights > 0) %>%                                                   # Drop samples outside the depth window
+  group_by(`CTD cast_`, Depth) %>% 
+  summarise(Ammonium = weighted.mean(`Ammonium_mM l-1`, weights),           # Weighted averages
+            Nitrate =  weighted.mean(`Nitrate+nitrite_mM l-1`, weights),
+            Samples = n()) %>%                                              # Number of samples contributing to each estimate
+  mutate(Ratio = Ammonium/Nitrate) %>%                                      # Get a ratio
+  st_join(domains) %>%                                                      # Check which are in the model domain
+  st_drop_geometry() %>%                                                    # Simplify the output
+  drop_na() %>% 
+  group_by(Shore, Depth) %>%                        
+  summarise(Ratio = weighted.mean(Ratio, Samples),                          # Calculate average, weighting by the number of samples
+            Casts = n())                                                    # Number of CTD casts contributing to each estimate
+
+saveRDS(final, "./Objects/Ammonia to nitrate ratio.rds")
