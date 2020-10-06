ecd908fd7e07dc1b5a5143d17060acf6ec4cf99b
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-10-06 17:42:56 +0100

    Updated annotations.

--- R scripts/saltless.05 DISTURBANCE.R
+++ R scripts/saltless.05 DISTURBANCE.R
@@ -6,23 +6,22 @@
 rm(list=ls())
 
 Packages <- c("MiMeMo.tools", "tidyverse", "sf", "exactextractr", "raster")# List packages
-lapply(Packages, library, character.only = TRUE)                 # Load packages
+lapply(Packages, library, character.only = TRUE)                           # Load packages
 source("./R scripts/@_Region file.R")
 
-habitats <- readRDS("./Objects/Habitats.rds") %>% 
+habitats <- readRDS("./Objects/Habitats.rds") %>%                          # Import maps of sea bed habitats
   filter(Habitat != "Rock")
 
-disturbance <- raster::brick("../Sediment/Output/Greenland_and_barents_sea_seasonal_stress.nc") # Import full sediment grid
+disturbance <- raster::brick("../Sediment/Output/Greenland_and_barents_sea_seasonal_stress.nc") # Import seasonal disturbance
 
 #### Calculate mean disturbance per habitat area, weighting by cell coverage ####
 
-values <- exact_extract(disturbance, habitats, fun = 'mean')
-names(values) <- month.name
+values <- exact_extract(disturbance, habitats, fun = 'mean')               # Mean habitat disturbance
+names(values) <- month.name                                                # Put months as column names
 
-result <- cbind(st_drop_geometry(habitats),
+result <- cbind(st_drop_geometry(habitats),                                # Bind to map metadata
                 values) %>% 
-  pivot_longer(January:December, names_to = "Month", values_to = "Disturbance")
-
+  pivot_longer(January:December, names_to = "Month", values_to = "Disturbance") # Reshape dataframe to long format
 
 saveRDS(result, "./Objects/Habitat disturbance.rds")
 
@@ -31,11 +30,7 @@ ggplot(result) +
   theme_minimal() +
   scale_linetype_manual(values = c("solid", "twodash", "dotted")) +
   scale_x_continuous(breaks = c(0, 3, 6, 9, 12)) +
-#  theme(panel.grid.major.x = element_blank(),
-#        legend.position = "top") +
-#  viridis::scale_fill_viridis(discrete = T, name = "Sediment class:") +
   labs(y = "Proportion of time disturbed", x = "Month") +
 NULL
 
 ggsave("./Figures/sediment/Habitat disturbance.png", width = 16, height = 8, units = "cm")
-
