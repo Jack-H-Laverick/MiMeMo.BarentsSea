d85d20ad357cbe6a7a168f9b75212eefa2f2db6b
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-10-29 09:37:24 +0000

    Use ICES landings to work out how much fishing activity is Russian.

---
+++ R scripts/fish.14 SCALE ICES.R
@@ -0,0 +1,33 @@
+
+## Ratio of Russian landings to non-by guild
+
+#### Set up ####
+
+rm(list=ls())                                                               # Wipe the brain
+
+packages <- c("tidyverse")                                                  # List packages
+lapply(packages, library, character.only = TRUE)                            # Load packages
+
+guild <- read.csv("./Data/MiMeMo fish guilds.csv") %>% 
+  dplyr::select(Guild, Category, Subcategory) %>% 
+  distinct
+
+#### Get an inflation factor for each guild to get from Non-Russian to International landings ####
+
+ICES <-read.csv("./Data/ICES fish/ICES_1903-2017.csv", header=T) %>%        # Import combined ICES landings  
+       filter(str_detect(area, "27.1|27.2.b.2|27.2.a.2|27.2.a_NK|27.2.b.NK|27.2_NK") # Limit to areas of interest
+              & area != "27.14.a") %>% 
+       mutate(Russia = str_detect(cname, "Russia")) %>%                     # Identify Russian activity
+       left_join(guild) %>%                                                 # Attach guilds
+       dplyr::select(Russia, Guild, tonnage) %>%                            # Limit to columns of interest
+       group_by(Guild, Russia) %>%                                          # Per guild and group of flags 
+       summarise(tonnage = sum(tonnage, na.rm = T)) %>%                     # Total the landings
+       ungroup() %>%                            
+       group_by(Guild) %>%                                                  # Now for each guild
+       mutate(total_guild_tonnage = sum(tonnage)) %>%                       # Total landings
+       ungroup() %>% 
+       filter(Russia == FALSE) %>%                                          # Don't need Russian data anymore
+       mutate(Inflation = total_guild_tonnage/tonnage) %>%                  # How do we get from non-Russian landings per guild to our known total?
+       dplyr::select(Guild, Inflation)
+
+saveRDS(ICES, "./Objects/ICES landings inflation.rds")
