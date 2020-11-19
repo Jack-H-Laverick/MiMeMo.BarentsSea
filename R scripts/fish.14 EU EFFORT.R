c8295b2a90e547b7e58b819ada662d7398e47942
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-11-19 09:17:14 +0000

    Scale fishing effort using GFW not area.

--- R scripts/fish.14 EU EFFORT.R
+++ R scripts/fish.14 EU EFFORT.R
@@ -10,9 +10,15 @@ lapply(c(packages), library, character.only = TRUE)                           # 
 
 source("./R scripts/@_Region file.R")                                         # Get region mask
 
+plan(multiprocess)
+
 Region_mask <- st_transform(Region_mask, crs = 4326)                          # reproject to match EU data
 
-Domains <- st_transform(readRDS("./Objects/Domains.rds"), crs = 4326)
+GFW_mobile <- brick("./Objects/GFW.nc", varname = "REST_mobile_gear") %>%     # Get mean fishing effort across years from Global fishing watch
+  calc(mean, na.rm = T)
+
+GFW_static <- brick("./Objects/GFW.nc", varname = "REST_static_gear") %>%     # For each class of gear
+  calc(mean, na.rm = T)
 
 domain <- st_transform(readRDS("./Objects/Domains.rds"), crs = 4326) %>%      # reproject to match EU data
   dplyr::select(-c(Shore, Elevation, area)) %>%                               # Drop unnecessary columns
@@ -23,74 +29,66 @@ domain <- st_transform(readRDS("./Objects/Domains.rds"), crs = 4326) %>%      # 
 
 gears <- read.csv("./Data/MiMeMo gears.csv")                                  # Load fishing gear classifications
 
-EU_effort <- rgdal::readOGR(dsn="./Data/EU fish/spatial_effort_2015-2018/") %>% # Import EU effort shapefile
+EU <- rgdal::readOGR(dsn="./Data/EU fish/spatial_effort_2015-2018/") %>%      # Import EU effort shapefile
   st_as_sf() %>%                                                              # Convert to SF
-  dplyr::select(year, quarter, ger_typ, rctngl_, ttfshdy, trgt_ss) %>%        # Drop some columns, ttfshdy is "total fishing days"
-  rename(Gear_code = ger_typ, FAO = trgt_ss)
+  dplyr::select(year, quarter, ger_typ, rctngl_, ttfshdy) %>%                 # Drop some columns, ttfshdy is "total fishing days"
+  rename(Gear_code = ger_typ)
+
+EU_Arctic <- st_contains(Region_mask, EU, sparse = F) %>%                     # Which EU polygons are in the model mask?
+  t() %>%                                                                     # Transpose to indicate row not columns
+  EU[.,] %>%                                                                  # Subset the Eu data spatially
+  left_join(gears) %>%                                                        # Attach gear classifications
+  filter(Aggregated_gear != "Dropped")  
 
 #### Locate EU effort inside habitat types ####
-
-EU_Arctic <- st_contains(Region_mask, EU_effort, sparse = F) %>%              # Which EU polygons are in the model mask?
-  t() %>%                                                                     # Transpose to indicate row not columns
-  EU_effort[.,] %>%                                                           # Subset the Eu data spatially
-  rownames_to_column(var = "EU_polygon") %>%                                  # Create a column to track each EU polygon
-  st_intersection(Domains) %>%                                                # Split the EU polygons cross habitat types
-  mutate(area_stat_rectangle = as.numeric(st_area(.))) %>%                    # Work out the area of all the pieces
-  st_drop_geometry() %>%                                                      # Drop geometry column for simplicity
-  drop_na(Shore) %>%                                                          # Drop shapes which fall outside of habitats
-  group_by(EU_polygon) %>%                                                    # Per original EU polygon
-  mutate(share = area_stat_rectangle / sum(area_stat_rectangle)) %>%          # Work out the proportion of the area in each piece split over habitats
-  ungroup() %>% 
-  mutate(effort_contributions = ttfshdy*share) %>%                            # Combine habitat labels
-  left_join(gears) %>%                                                        # Attach gear classifications
-  filter(Aggregated_gear != "Dropped") %>% 
-  group_by(Aggregated_gear, year) %>%                                         # By year and gear
-  summarise(effort = sum(effort_contributions, na.rm = TRUE)) %>%             # total the fishing effort
-  drop_na() %>%                                                               # Drop unassigned gears
-  summarise(effort = mean(effort, na.rm = TRUE)) %>%                          # Average for each gear across years
-  mutate(effort = effort*24)                                                  # Convert days to hours
-
-#saveRDS(EU_Arctic, "./Objects/EU absolute effort by gear.rds")
-
-
-## test
-
-library(furrr)
-plan(multiprocess)
-
-test <- st_contains(Region_mask, EU_effort, sparse = F) %>%                   # Which EU polygons are in the model mask?
-  t() %>%                                                                     # Transpose to indicate row not columns
-  EU_effort[.,] %>%                                                           # Subset the Eu data spatially
-  rownames_to_column(var = "EU_polygon") %>%                                  # Create a column to track each EU polygon
-  split(f = list(.$EU_polygon)) %>%                                           # Isolate each shape for fast paralel processing
-  future_map( ~ {bind_rows(.x, rename(st_sf(domain), geometry = domain)) %>%  # Bind the domain polygon to the object 
-    st_intersection() %>%                                                     # Split the polygons by areas of overlap
-    mutate(area_stat_rectangle = as.numeric(st_area(.))) %>%                  # Calculate the area of each piece
-    st_drop_geometry() %>%                                                    # Drop geometry column for simplicity
-    drop_na() %>%                                                             # Drop the model domain area which isn't under the fishing polygon
-    mutate(weight = area_stat_rectangle/sum(area_stat_rectangle, na.rm = T)) %>% # Calculate the proportion each piece of fishing area represents
-    filter(n.overlaps == 2)                                                   # Limit to the piece where domain and fishing overlapped
-    }, .progress = T) %>%
-  data.table::rbindlist() %>%                                                 # Bind each fishing feature back into a DF
-  mutate(effort_contributions = ttfshdy*weight) %>%                           # Scale fishing effort by area in the model domain
-  left_join(gears) %>%                                                        # Attach gear classifications
-  filter(Aggregated_gear != "Dropped") %>% 
-  group_by(Aggregated_gear, year) %>%                                         # By year and gear
-  summarise(effort = sum(effort_contributions, na.rm = TRUE)) %>%             # total the fishing effort
-  drop_na() %>%                                                               # Drop unassigned gears
-  summarise(effort = mean(effort, na.rm = TRUE)) %>%                          # Average for each gear across years
-  mutate(effort = effort*24)                                                  # Convert days to hours
 
 target <- dplyr::select(gears, Aggregated_gear) %>%                           # Select gear names
   distinct() %>%                                                              # Drop duplicates
   filter(Aggregated_gear != "Dropped")                                        # Drop unused gears
 
-Goal <- test %>%                                                              # Total fishing effort
+tictoc::tic()
+corrected_effort <- rownames_to_column(EU_Arctic, var = "EU_polygon") %>%      # Create a column to track each IMR region and gear combination
+  split(f = list(.$EU_polygon)) %>%                                           # Isolate each shape for fast paralel processing
+  future_map( ~ {bind_rows(.x, rename(st_sf(domain), geometry = domain)) %>%  # Bind the domain polygon to the object 
+    st_intersection() %>%                                                     # Split the polygons by areas of overlap
+    mutate(GFW = ifelse(Gear_type == "Mobile", exact_extract(GFW_mobile, ., fun = "sum"),          # Get the GFW fishing effort in each shape
+                                               exact_extract(GFW_static, ., fun = "sum"))) %>% 
+    st_drop_geometry() %>%                                                    # Drop geometry column for simplicity
+    drop_na() %>%                                                             # Drop the model domain area which isn't under the fishing polygon
+    mutate(weight = GFW/sum(GFW, na.rm = T)) %>%                              # Calculate the proportion each piece of fishing area represents
+    filter(n.overlaps == 2)                                                   # Limit to the piece where domain and fishing overlapped
+    }, .progress = T) %>%
+  data.table::rbindlist() %>%                                                 # Bind each fishing feature back into a DF
+  mutate(effort_contributions = ttfshdy*weight*24)                            # Scale fishing effort by area in the model domain
+                                                                              # Convert days to hours
+saveRDS(corrected_effort, "./Objects/EU corrected pixel fishing effort")      # Save
+
+#### Summarise ####
+  
+summary <- group_by(corrected_effort, Aggregated_gear, year) %>%              # By year and gear
+  summarise(effort = sum(effort_contributions, na.rm = TRUE)) %>%             # total the fishing effort
+  drop_na() %>%                                                               # Drop unassigned gears
+  summarise(effort = mean(effort, na.rm = TRUE)) %>%                          # Average for each gear across years
   right_join(target) %>%                                                      # Reintroduce unobserved gears
   replace_na(replace = list(effort = 0)) %>%                                  # Nas are actually effort of 0
   column_to_rownames('Aggregated_gear') %>%                                   # Remove character column
   as.matrix() %>%                                                             # Convert to matrix
   .[order(row.names(.)),]                                                     # Alphabetise rows to ensure a match with other objects
+tictoc::toc()
 
-saveRDS(Goal, "./Objects/EU absolute fishing effort")                         # Save
+saveRDS(summary, "./Objects/EU absolute fishing effort")                      # Save
 
+#### Visualise ####
+
+library(ggnewscale)
+library(stars)
+
+star <- st_as_stars(GFW_mobile)    # convert GFW to stars objects for use in sf plots
+star2 <- st_as_stars(GFW_static)
+
+ggplot() +
+  geom_sf(data = domain, fill = "yellow", colour = "black") +
+  geom_sf(data = EU_Arctic, aes(fill = ttfshdy), alpha = 0.3, colour = NA) +
+  new_scale("fill") +
+  geom_stars(data=star) +
+  scale_fill_continuous(na.value = NA, low = "purple", high = "limegreen", trans = "sqrt")
