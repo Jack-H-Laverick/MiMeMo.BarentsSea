58065e5cc7aba1c501fbcbd5fd96c7f60c4ec5ca
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-12-08 17:07:38 +0000

    Move deleting of duplicated StrathE2E files into it's own script to reduce friction during development.

---
+++ R scripts/strathE2E.05 MODEL CLEANUP.R
@@ -0,0 +1,5 @@
+
+# Remove the files which have been replaced by ones for the new region
+unlink("./StrathE2E/Models/Barents Sea/2011-2019/Driving/chemistry_NORTH_SEA_1970-1999.csv")
+unlink("./StrathE2E/Models/Barents Sea/2011-2019/Param/physical_parameters_NORTH_SEA.csv")
+unlink("./StrathE2E/Models/Barents Sea/2011-2019/Driving/physics_NORTH_SEA_1970-1999.csv") # Delete old file
