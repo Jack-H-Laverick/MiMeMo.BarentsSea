8daaa4ccb03b3c5f5d8ae125a7c75d6e9ba992b2
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-01-05 15:50:46 +0000

    updated disturbance rate calculations to daily average per month.

--- R scripts/saltless.07 DISTURBANCE.R
+++ R scripts/saltless.07 DISTURBANCE.R
@@ -12,20 +12,70 @@ source("./R scripts/@_Region file.R")
 habitats <- readRDS("./Objects/Habitats.rds") %>%                          # Import maps of sea bed habitats
   filter(Habitat != "Rock")
 
-disturbance <- brick("../Sediment/Output/Greenland_and_barents_sea_seasonal_disturbance.nc") # Import seasonal disturbance
+data <- list(gravel = brick("../Sediment/Output/Daily_disturbance_gravel.nc"), # Import daily disturbance for sediment fractions
+             sand = brick("../Sediment/Output/Daily_disturbance_sand.nc"),              
+             silt = brick("../Sediment/Output/Daily_disturbance_silt.nc"))              
 
-#### Calculate mean disturbance per habitat area, weighting by cell coverage ####
+# Import sediment fraction proportions at each pixel
 
-values <- exact_extract(disturbance, habitats, fun = 'mean')               # Mean habitat disturbance
-names(values) <- month.name                                                # Put months as column names
+weights <- map(c("Rock", "Gravel", "Sand", "Silt"), ~{
+  
+weights <- raster("../Sediment/Output/Greenland_and_barents_sea_shelf_sediments.nc",
+                  varname = .x) %>% 
+  projectRaster(data[["silt"]])
 
-result <- cbind(st_drop_geometry(habitats),                                # Bind to map metadata
-                values) %>% 
-  pivot_longer(January:December, names_to = "Month", values_to = "Disturbance") # Reshape dataframe to long format
+  weights[is.na(weights)] <- 0 
 
-saveRDS(result, "./Objects/Habitat disturbance.rds")
+  return(weights)
+})
 
-ggplot(result) +
+#### Get variable to label the day of each raster layer ####
+
+raw <- ncdf4::nc_open("../Sediment/Objects/tides.nc")                      # Open file containing all the data
+time <- raw$dim$time$len                                                   # Pull the length of the time dimension
+ncdf4::nc_close(raw)                                                       # Close file
+
+days <- seq(ISOdate(2003, 02, 1, 0), by = "2 hours", length.out = time) %>%# Calculate time steps for the dataset
+  format("%Y%m%d") %>% 
+  unique()
+
+#### Calculate mean disturbance per habitat area, weighting by cell coverage and sediment fraction proportion ####
+
+scale <- exact_extract(stack(weights), habitats, fun = 'mean') %>%         # Mean habitat disturbance weighted by sediment fraction per pixel
+  setNames(c("Rock", "Gravel", "Sand", "Silt")) %>% 
+  cbind(st_drop_geometry(habitats)) %>% 
+  pivot_longer(-c(Habitat, Shore), names_to = "Fraction", values_to = "Proportion")
+
+mobile <- map2_df(data, weights[2:4], ~{
+  
+values <- exact_extract(.x, habitats, fun = 'weighted_mean', weights = .y) %>% # Mean habitat disturbance weighted by sediment fraction per pixel
+  t() %>%                                                                  # Swap habitats to columns and days to rows
+  as.data.frame() %>%                                         
+  setNames(paste0(habitats$Habitat, " ", habitats$Shore)) %>%              # Get habitat types as column names
+  rownames_to_column(var = "Day") %>%                                      # Get day as a column
+  mutate(Day = days[as.numeric(str_remove(Day, "weighted_mean.X"))],       # Clean the column
+         Fraction = names(.y)) %>% 
+  pivot_longer(-c(Day, Fraction), names_to = "Habitat", values_to = "Disturbance") %>%  # Collect habitat types into a column
+  separate(Habitat, into = c("Habitat", "Shore"))                          # Regain the habitat meta-data
+  }) %>% separate(Fraction, into = c(NA,NA,NA, "Fraction")) %>% 
+  mutate(Fraction = str_to_title(Fraction))
+
+rock_summary <- filter(mobile, Fraction == "Gravel") %>% 
+  mutate(Fraction = "Rock",
+         Disturbance = 0)
+
+summary <- bind_rows(mobile, rock_summary) %>%                             # Add in Rock disturbance (0)
+  left_join(scale) %>%                                                     # Attach weights of sediment classes within habitats
+  group_by(Habitat, Shore, Day) %>% 
+  summarise(Disturbance = weighted.mean(Disturbance, Proportion)) %>%      # Get the habitat-wide average by averaging sediment specific rates by sediment proportions per day
+  ungroup() %>% 
+  mutate(Month = lubridate::month(as.Date(Day, format = "%Y%m%d"))) %>%    # Convert days to months
+  group_by(Month, Habitat, Shore) %>% 
+  summarise(Disturbance = mean(Disturbance))                               # Average over months
+
+saveRDS(summary, "./Objects/Habitat disturbance.rds")
+
+ggplot(summary) +
   geom_line(aes(x = as.numeric(as.factor(Month)), y = Disturbance, colour = Shore, linetype = Habitat)) +
   theme_minimal() +
   scale_linetype_manual(values = c("solid", "twodash", "dotted")) +
