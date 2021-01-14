6dc1a5605092a1ed8425ff7f181b331f8d8b54aa
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-01-14 13:13:50 +0000

    fix: Changed imported bands so that NEMO-MEDUSA and SINMOD are compared at the same depths.

--- R scripts/flows.01 VC-SINMOD.R
+++ R scripts/flows.01 VC-SINMOD.R
@@ -63,7 +63,7 @@ tictoc::tic()
 NM <- list.files("/mnt/idrive/Science/MS/Shared/CAO/mimemo/clipped_medusa/", 
                         recursive = T, full.names = TRUE, pattern = "grid_W") %>%
   future_map(~{
-    raster(.x, band = 1, varname = "votkeavt") %>%           # Depth is a band (3rd dimension)
+    raster(.x, band = 9, varname = "votkeavt") %>%           # Depth is a band (3rd dimension)
       as.data.frame(na.rm = T, xy = T)}, .progress = T) %>% 
   rbindlist() %>% 
   setNames(c("x", "y", "vertical_diffusivity"))
@@ -71,14 +71,16 @@ NM <- list.files("/mnt/idrive/Science/MS/Shared/CAO/mimemo/clipped_medusa/",
 setDT(NM, key = c("x", "y"))
 
 NM <- NM[NM_space]                                                # Drop points which don't match the space object 
+NM <- NM[vertical_diffusivity != 0]
+
 tictoc::toc()
 
 ggplot() +
-  geom_density(data = NM, aes(vertical_diffusivity), fill = "orange") +
-  geom_density(data = SINMOD, aes(vertical_diffusivity), fill = "blue") +
-  geom_density(data = NM, aes(vertical_diffusivity), fill = "orange", alpha = 0.6) +
-  geom_density(data = SINMOD, aes(vertical_diffusivity)) +
-  ggtext::geom_richtext(aes(x = 0.053, y = 47), fill = NA, label.color = NA, label.padding = grid::unit(rep(0, 4), "pt"),
+  geom_density(data = NM, aes(vertical_diffusivity), fill = "orange", size = 0.2) +
+  geom_density(data = SINMOD, aes(vertical_diffusivity), fill = "blue", size = 0.2) +
+  geom_density(data = NM, aes(vertical_diffusivity), fill = "orange", alpha = 0.6, size = 0.2) +
+  geom_density(data = SINMOD, aes(vertical_diffusivity), size = 0.2) +
+  ggtext::geom_richtext(aes(x = 2, y = 25), fill = NA, label.color = NA, label.padding = grid::unit(rep(0, 4), "pt"),
                         hjust = 0, vjust = 1, size = 4, 
                label = str_glue("<b style = 'color:orange'>NEMO-MEDUSA:</b> 
                mean = {round(mean(NM$vertical_diffusivity, na.rm = T), 3)}<br>
@@ -87,7 +89,7 @@ ggplot() +
   theme(plot.caption = ggtext::element_markdown()) +
   labs(caption = "Density distribution of vertical diffusivity values for
        <b style = 'color:orange'>NEMO-MEDUSA</b> and <b style = 'color:blue'>SINMOD</i></b>
-       model outputs",
+       model outputs at 10m",
        x= "Vertical diffusivity") + 
    NULL
 
@@ -154,47 +156,3 @@ quantile_plot <- data.frame(quantile = seq(0.1, 1, 0.01)) %>%
 ggplot(quantile_plot) +
   geom_line(aes(x = quantile, y = value)) +
   theme_minimal()
-
-#### Check pre crop ####
-
-# file <- "/mnt/idrive/Science/MS/Shared/CAO/nemo/ALLARC/1980/grid_W_19800105.nc" 
-# 
-# check <- raster(file, band = 1, varname = "votkeavt")           # Depth is a band (3rd dimension)
-# check[check == max(values(check))] <- NA
-# 
-# check[check == max(values(check), na.rm = T)] <- NA
-# 
-# check <- as.data.frame(check, xy = T, na.rm = T)
-# 
-# ggplot() +
-#   geom_raster(data = check, aes(x=x, y=y, fill = vertical.eddy.diffusivity))
-# 
-# raw <-nc_open(file)
-# 
-# array <- ncvar_get(raw, "votkeavt", start = c(1,1,1,1), count = c(-1,-1,-1, -1))
-# array[array == max(array)] <- NA
-# 
-# array[array == max(array, na.rm = T)] <- NA
-# 
-# quantile_plot <- data.frame(quantile = seq(0.1, 0.9, 0.01)) %>% 
-#   mutate(value = quantile(array, probs = quantile, na.rm = T))
-# 
-# ggplot(quantile_plot) +
-#   geom_line(aes(x = quantile, y = value)) +
-#   theme_minimal()
-
-
-
-##### 1 D#####
-
-raw <-nc_open(file)
-
-array <- ncvar_get(raw, "votkeavt", start = c(110,75,1,1), count = c(1,1,-1, -1))
-
-nc_close(raw)
-
-ggplot() +
-  geom_line(aes(x = 1:75, y = array)) +
-  geom_point(aes(x = 1:75, y = array)) +
-  coord_flip() +
-  scale_x_reverse()
