7ba0a35541e514b166d926e484c44a8ee150b8ff
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-01-29 15:49:30 +0000

    feat: update to slabR approach.

--- R scripts/nm/nemo-medusa.02 EXTRACTION.R
+++ R scripts/nm/nemo-medusa.02 EXTRACTION.R
@@ -1,12 +1,12 @@
 
 # Pull the contents of netcdf files: Remember to mount the idrive by typing midrive into the Konsole
-# saveRDS("./Objects/Months.")  # Marker so network script can see where the data is being saved too, it's buried in a function
+# saveRDS("./Objects/Months/.")  # Marker so network script can see where the data is being saved too, it's buried in a function
 
 #### Set up ####
 
 rm(list=ls())                                                               # Wipe the brain
 
-packages <- c("nemomedusR", "tidyverse", "data.table", "sf", "tictoc", "furrr", "ncdf4") # List packages
+packages <- c("nemomedusR", "tidyverse", "sf", "furrr", "ncdf4")            # List packages
 lapply(packages, library, character.only = TRUE)                            # Load packages
 source("./R scripts/@_Region file.R")                                       # Define project region 
 
@@ -23,92 +23,55 @@ all_files[which(all_files$Date == "_2047105"),]$Type <- "grid_T_"           # On
 all_files[which(all_files$Date == "_2047105"),]$Date <- "20471005"          
 
 all_files  <- all_files %>%
-  separate(Date, into = c("Year", "Month", "Day"),sep = c(4, 6)) %>%        # Extract the year and month from the file name 
+  separate(Date, into = c("Year", "Month", NA),sep = c(4, 6)) %>%           # Extract the year and month from the file name 
   mutate(Year = as.integer(Year),                                           # Set time as integers 
-         Month = as.integer(Month), 
-         Day = as.integer(Day))
-
-examples <- group_by(all_files, Type) %>% slice(1)                          # Get one example for each file type
-trial <- group_by(all_files, Type) %>% slice(1:3)                           # Get one example for each file type
-
+         Month = as.integer(Month)) %>%
+  filter(Type != "grid_W_")                                                 # Drop the vertical water movement files
+  
 domains <- readRDS("./Objects/Domains.rds") %>%                             # Load SF polygons of the MiMeMo model domains
-  select(-c(Elevation, area))                                               # Drop uneeded data which would get included in new NM files
+  select(-c(Elevation, area))                                               # Drop unneeded data which would get included in new NM files
 
-#### Set the spatial environment ####
+crop <- readRDS("./Objects/Domains.rds") %>%                                # Load SF polygons of the MiMeMo model domains
+  st_buffer(dist = 50000) %>%                                               # It needs to be a bit bigger for sampling flows at the domain boundary
+  summarise() %>%                                                           # Combine polygons to avoid double sampling
+  mutate(Shore = "Buffer")
 
-Space <- get_spatial(paste0(examples[1,"Path"], examples[1,"File"]))        # Pull space from the first file
+Bathymetry <- readRDS("./Objects/Fixed_grid2.rds") %>%                      # Import NEMO-MEDUSA bathymetry
+  st_drop_geometry() %>%                                                    # Drop sf geometry column 
+  select(-c("x", "y"), latitude = Latitude, longitude = Longitude)          # Clean column so the bathymetry is joined by lat/lon
 
-W <- nc_open(paste0(examples[4,"Path"], examples[4,"File"]))                # Get the different depth vector for W files
-DepthsW <- W$dim$depthw$vals ; nc_close(W) ; rm(W) 
+#### Build summary scheme ####
 
-# Speed things up with a tighter crop
-test <- Space$nc_lat > 65 & Space$nc_lat < 84 & Space$nc_lon > 6 & Space$nc_lon < 68
-E <- min(which(test == TRUE, arr.ind = TRUE)[,"row"])
-W <- max(which(test == TRUE, arr.ind = TRUE)[,"row"])
-S <- min(which(test == TRUE, arr.ind = TRUE)[,"col"])
-N <- max(which(test == TRUE, arr.ind = TRUE)[,"col"])
-Limits <- data.frame("x_start" = E, "x_count" = W - E + 1, "y_start" = S, "y_count" = N - S + 1)
+scheme <- scheme_strathE2E(get_spatial(paste0(all_files$Path[1], all_files$File[1]), grid_W = F),
+                           Bathymetry, 60, 400, crop) %>% 
+  select(x, y, layer, group, weight, slab_layer, longitude, latitude, Bathymetry) %>%   # Get a scheme to summarise for StrathE2E
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326, remove = F) %>% # Convert to sf object
+  st_join(st_transform(domains, crs = 4326)) %>%                            # Attach model zone information
+  st_drop_geometry()                                                        # Drop sf formatting
 
-Space$shallow <- between(Space$nc_depth[1:38], 0, 60)                       # Find the positions in the depth vector for shallow slice
-Space$deep <- Space$nc_depth[1:38] > 60                                     # Find the elements in the array to extract for deep slice
-Space$shallow_W <- between(DepthsW[1:39], 0, 60)                            # And for W files
-Space$deep_W <- DepthsW[1:39] > 60                            
+start <- scheme_to_start()                                                  # Get netcdf vectors which define the minimum
+count <- scheme_to_count()                                                  # amount of data to import
+scheme <- scheme_reframe(scheme) 
 
-output <- readRDS("./Objects/Fixed_grid2.rds")                              # Load in Bathymetry
-bathymetry <- matrix(abs(output$Bathymetry), Limits$x_count, Limits$y_count)
+ice_scheme <- filter(scheme, layer == 1) %>%                                # Ice data is stored as a matrix, so needs a new scheme
+  arrange(group) %>% 
+  transmute(n = xyindex_to_nindex(x, y, count[1]))
 
-Space$start3D <- c(Limits$x_start,Limits$y_start,1,1) ; Space$count3D <- c(Limits$x_count, Limits$y_count,38,-1)                           # Spatial cropping at import for variables with depths shallower than 400 m
-Space$start3DW <- c(Limits$x_start,Limits$y_start,1,1) ; Space$count3DW <- c(Limits$x_count, Limits$y_count,39,-1)                         # Spatial cropping at import for variables with depths shallower than 400 m (W files)
+scheme_result <- arrange(scheme, group) %>%                                 # Create a meta-data object to attach to the summaries
+  select(x, y, slab_layer, longitude, latitude, Shore, Bathymetry) %>% 
+  distinct() %>% 
+  mutate(slab_layer = if_else(slab_layer == 1, "S", "D"),
+         weights = case_when(slab_layer == "S" & Bathymetry >= 60 ~ 60,     # Weights for zonal averages by thickness of water column
+                             slab_layer == "S" & Bathymetry < 60 ~ Bathymetry,
+                             slab_layer == "D" & Bathymetry >= 400 ~ 340,
+                             slab_layer == "D" & Bathymetry < 400 ~ (Bathymetry - 60)))
 
-Space$s.weights <- get_weights(0, 60, bathymetry) #Add depths as function argument    # Work out water column proportions for weighted means                                                      
-Space$d.weights <- get_weights(60, 400, bathymetry)
-Space$s.weights_W <- get_weights.W(0, 60, bathymetry)                       # And for W files                                                      
-Space$d.weights_W <- get_weights.W(60, 400, bathymetry)
+#### extract ####
 
-Window <- st_join(output, domains) %>% 
-  MiMeMo.tools::sfc_as_cols(names = c("x_crs", "y_crs")) %>% 
-  st_drop_geometry() %>% 
-  filter(between(x_crs, lims[["xmin"]], lims[["xmax"]]) &                   # Clip to plotting window
-         between(y_crs, lims[["ymin"]], lims[["ymax"]])) %>%                
-  select(-c(x_crs, y_crs)) %>%                                              # Drop columns for cropping to plot window
-  mutate(Depth = "S") %>%                                                   # Add a depth column
-  bind_rows(., mutate(., Depth = "D")) %>%                                  # Duplicate entries for second depth layer
-  anti_join(filter(., Depth == "D" & Shore == "Inshore")) %>%               # Remove Inshore deep which doesn't exist                 
-  mutate(weights = abs(Bathymetry)) %>%                                     # Get weights for pixel when calculating compartment averages
-  mutate(weights = ifelse(Depth == "S" & weights > 60, 60,                  # Set shallow pixel thickness to 60 m deep, even if there is deep inshore pixels close to shore 
-                    ifelse(Depth == "D", weights - 60, weights)))           # If it's a deep pixel reduce thickness by 60 m, otherwise keep thickness as is 
-
-output <- st_drop_geometry(output) %>% 
-  bind_rows(., .) %>% 
-  mutate(Depth = as.factor(rep(c("S", "D"), each = (nrow(.)/2))))           # Recode depths from numeric to character
-
-#### Build the monthly sumaries ####
-
-tic("Creating monthly data objects from netcdf files")                     # Time the data extraction
- 
- overnight <- all_files %>%
-   split(., f = list(.$Month, .$Year)) %>%                                  # Get a DF of file names for each time step to summarise to
-   .[sapply(., function(x) dim(x)[1]) > 0] %>%                              # Drop empty dataframes (Months which weren't observed but split introduces)
-#   .[1429:1440] %>% 
-   future_map(safely(NEMO_MEDUSA), crop = Window, analysis = "StrathE2E",
-              summary = output, space = Space, out_dir = "./Objects/Months",
-              .progress = T)                                                # Perform the extraction and save an object for each month (in parallel)
- toc()                                                                      # Stop timing
-
-# 21 hours
- 
-# saveRDS(overnight, "./Objects/NM-SE2E errors.rds")
-#look <- readRDS("./Objects/NM-SE2E errors.rds") 
-# errors <- map(overnight, `[[`, "error") # select only the error message
-# 
-# errors <- errors[-which(sapply(errors, is.null))] # drop nukks
-# 
-# length(errors)/1440 #43% had errors
-# 
-# problem <- unlist(map(errors, `[[`, "message")) # select only the error message
-# 
-# problem <- unique(unlist(map(errors, `[[`, "message"))) # select only the error message
-
-
-
-
+tictoc::tic()
+all_files %>%
+  split(., f = list(.$Month, .$Year)) %>%                                   # Specify the timestep to average files over.
+  future_map(NEMO_MEDUSA, analysis = "slabR", summary = scheme_result,
+             scheme = scheme, ice_scheme = ice_scheme$n, start = start,  
+             count = count, out_dir = "./Objects/Months", .progress = T)    # Perform the extraction and save an object for each month (in parallel)
+tictoc::toc()
