0b291aaf948cac708d9b35f9cc026522e95f4289
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-02-03 09:12:35 +0000

    feat: implemented ice_threshold.

--- R scripts/nm/nemo-medusa.05 TIME SERIES.R
+++ R scripts/nm/nemo-medusa.05 TIME SERIES.R
@@ -8,14 +8,16 @@ rm(list=ls())                                                                 # 
 
 library(MiMeMo.tools)                              
 library(furrr)
-plan(multisession)                                                    # Instructions for parallel processing
+plan(multisession)                                                            # Instructions for parallel processing
 
 #### Extract time series ####
 
 tic("Creating time series by compartment")                                    # Time the data extraction
 
 TS <- list.files("./Objects/Months/", full.names = T) %>%                     # Get list of NEMO-MEDUSA files
-  future_map(summarise_ts, .progress = T) %>%                                 # Read in the months and calculate mean compartments
+  future_map(NM_volume_summary,                                               # Read in the months and calculate mean compartments
+             ice_threshold = 0.15,                                            # Treating pixels with ice concentrations below 15% as ice_free
+             .progress = T) %>%      
   data.table::rbindlist() %>%                                                 # Combine timesteps into series
   mutate(date = as.Date(paste("15", Month, Year, sep = "/"), format = "%d/%m/%Y"), # Create a single date column for plotting
          Compartment = paste(Shore, slab_layer, sep = " ")) %>%               # Build a single compartment column for plotting by
