c9bc2150316fbb367d3340291ebbab27ff0f2e54
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-02-12 10:30:43 +0000

    fix: readded distance form shore code which had been moved into a note.

--- R scripts/bathymetry/bathymetry.03 DEFINE DOMAIN.R
+++ R scripts/bathymetry/bathymetry.03 DEFINE DOMAIN.R
@@ -12,20 +12,42 @@ source("./R scripts/@_Region file.R")                                       # De
 world <- ne_countries(scale = "medium", returnclass = "sf") %>%             # Get a world map
   st_transform(crs = crs)                                                   # Assign polar projection
 
-Distance <- readRDS("./Objects/Bathymetry and distance from shore.rds")     # Read in bathymetry points with distances
+#### Distance from shore ####
+
+shrink <- filter(world, subregion %in% c("Northern America", "Northern Europe", "Eastern Europe")) # Measuring distances goes faster if we don't check every country
 
 Bathymetry <- readRDS("./Objects/Bathymetry_points.rds") %>%                # Get bathymetry
   .[seq(1, nrow(.), 16),] %>%                                               # Reduce resolution for plotting speed
-  filter(between(Elevation, -1000, 0)) %>%                                  # Clip area
-  st_as_stars()                                                             # Convert to stars to get cells instead of points
+  filter(between(Elevation, -1000, 0))  
+
+close <- st_as_sf(Bathymetry, coords = c("Longitude", "Latitude"), crs = 4326) %>% # Set dataframe to SF format
+  st_transform(crs) 
+
+dist <- st_distance(close, shrink) %>% pbapply::pbapply(1, min)             # Calculate the distances between points and polygons, grab the closest
+close$Shore_dist <- dist                                                    # Send distances to a column
+
+ggplot() +
+  geom_sf(data = close, aes(geometry = geometry, colour = Shore_dist), size = 0.1) +
+  geom_sf(data = world, size = 0.1) +
+  theme_minimal() +
+  theme(axis.text = element_blank()) +
+  viridis::scale_colour_viridis(name = 'Distance (m)') +
+  guides(colour = guide_colourbar(barwidth = 0.5, barheight = 15)) +
+  labs(caption = "Distance from shore") +
+  zoom +
+  NULL
+
+ggsave_map("./Figures/bathymetry/Distnace.png", last_plot())
+
+#### Final area choices ####
+
+Bathymetry <- st_as_stars(Bathymetry)                                       # Convert to stars to get cells instead of points
 st_crs(Bathymetry) <- st_crs(4326)                                          # set lat-lon crs
 
 Bathymetry <- st_as_sf(Bathymetry, as_points = F, merge = F)  %>%           # Convert the stars grid into SF polygons which can be merged
   st_transform(crs = crs) %>% 
-  st_join(dplyr::select(Distance, -Elevation)) %>%                          # Attach distance from shore for simultaneous filtering
+  st_join(dplyr::select(close, -Elevation)) %>%                             # Attach distance from shore for simultaneous filtering
   drop_na()
-
-#### Final area choices ####
 
 Domains <- mutate(Bathymetry, Shore = ifelse(between(Elevation, -400, -60) & Shore_dist > 20000, "Offshore",
                                     ifelse(Elevation > -60 | Shore_dist < 20000, "Inshore", NA))) %>% 
